a6e853b5c3af779d9387797304e650548103ec5b
savir-nosaj <janasty30@gmail.com>
2025-04-16 00:02:59 -0700

    Update ToDoList.jsx

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -3,7 +3,7 @@ import ToDoListItem from "./ToDoListItem";
 function ToDoList({todoList}) {
 
     return (
-        // code to add ternary statement
+        // code to add ternary statement - will continue
         <ul>
             {todoList.map(todo => <ToDoListItem key = {Date.now()} todo = {todo}/>)}
         </ul>
